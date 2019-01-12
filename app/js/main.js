decd95b29b633e22aaaac7f92b0676e912b91d88
Karolina Jędrzejak <kjedrzejak91@gmail.com>
2019-01-12 21:53:33 +0100

    6th commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,66 +1,74 @@
 $(document).ready(function(){
 
 
-const slideShow = $('.slide-show');
-const slides = $('.single-slide');
-const prev = $('.slider-nav-prev');
-const next = $('.slider-nav-next');
-const close = $('.slider-close');
-let slideCount = slides.length;
-let slideWidth = 100/slideCount;
-let slideIndex = 0;
-
-function slide(newSlideIndex) {
-   if(newSlideIndex < 0 || newSlideIndex > slideCount - 1) {
-      return;
-   }
-
-   let marginLeft = (newSlideIndex * (-100)) + '%';
-
-   slideShow.animate({
-      'margin-left': marginLeft
-   },1000);
-
-   slideIndex = newSlideIndex;
-}
+   const slideShow = $('.slide-show');
+   const slides = $('.single-slide');
+   const prev = $('.slider-nav-prev');
+   const next = $('.slider-nav-next');
+   const close = $('.slider-close');
+   let slideCount = slides.length;
+   let slideWidth = 100/slideCount;
+   let slideIndex = 0;
 
 
-slideShow.css({'width': slideCount + '00%'});
-
-slides.each(function(ind){
-   $(this).css({
-      'width': slideWidth + '%',
-      'margin-left': ind * slideWidth + '%'
+   
+   $('.staff-box').click(function(){
+      $(this).data("index");
+      $('.slider-container').show();
+      $('.container').hide();
+      $('#main-header').hide();
+   }); 
+   
+   close.click(function(){
+      $('.slider-container').hide();
+      $('.container').show();
+      $('#main-header').show();
    });
-});
-
-prev.on('click',function(){
-   slide( slideIndex - 1 );
-});
-
-next.on('click',function(){
-   slide( slideIndex + 1 );
-});
 
 
-$('.staff-box').click(function(){
-   $('.slider-container').show();
-   $('.container').hide();
-   $('#main-header').hide();
-}); 
-
-close.click(function(){
-   $('.slider-container').hide();
-   $('.container').show();
-   $('#main-header').show();
-});
+   
+   function slide(newSlideIndex) {
+      if(newSlideIndex < 0 || newSlideIndex > slideCount - 1) {
+         return;
+      }
 
 
 
 
-$(document).keydown(function(event) {
-   alert('You pressed ' + event.keyCode);
-   event.preventDefault();
- });
 
-});
+
+      
+   
+       let marginLeft = (newSlideIndex * (-100)) + '%';
+   
+      slideShow.animate({
+         'margin-left': marginLeft
+      },1000);
+   
+      slideIndex = newSlideIndex;
+    }
+   
+   
+   slideShow.css({'width': slideCount + '00%'});
+   
+   slides.each(function(ind){
+      $(this).css({
+         'width': slideWidth + '%',
+         'margin-left': ind * slideWidth + '%'
+      });
+   });
+   
+   prev.on('click',function(){
+      slide( slideIndex - 1 );
+   });
+   
+   next.on('click',function(){
+      slide( slideIndex + 1 );
+   });
+   
+
+   
+
+   
+   });
+   
